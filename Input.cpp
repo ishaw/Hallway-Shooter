7c5f6b19699be0f7fad9518e84f76c33ce728b9b
Scott Prager <hakusa@gmail.com>
2011-06-20 18:28:44 -0400

    Added Mouse class. INCOMPLETE/UNTESTED

--- Input.cpp
+++ Input.cpp
@@ -1,6 +1,8 @@
 
 #include "Input.h"
 
+#include <functional>
+
 Keyboard::Keyboard()
 {
     std::fill_n( states, irr::KEY_KEY_CODES_COUNT, false );
@@ -28,4 +30,61 @@ bool Keyboard::key_pressed( char c )
         c -= 0x20;
 
     return states[ c ];
+}
+
+Mouse::Mouse()
+    : wheelPos( 0 )
+{
+    std::fill_n( buttons, N_BUTTONS, RELEASED );
+}
+
+bool Mouse::OnEvent( const irr::SEvent& e )
+{
+    if( e.EventType == irr::EET_MOUSE_INPUT_EVENT )
+    {
+        typedef std::function< void(int,KeySate) > KeyFunc;
+        KeyFunc on_change = [&]( int i, KeySate inputState )
+        {
+            int other = inputState ^ ( PRESSED | RELEASED );
+            if( buttons[ i ] & other )
+                buttons[ i ] = KeySate( inputState | ACTIVE );
+            else
+                // Unsets the ACTIVE bit if set.
+                buttons[ i ] = KeySate( buttons[i] & inputState ); 
+        };
+
+
+        switch( e.MouseInput.Event )
+        {
+          case irr::EMIE_LMOUSE_PRESSED_DOWN: on_change( LEFT,   PRESSED  ); break;
+          case irr::EMIE_RMOUSE_PRESSED_DOWN: on_change( RIGHT,  PRESSED  ); break;
+          case irr::EMIE_MMOUSE_PRESSED_DOWN: on_change( MIDDLE, PRESSED  ); break;
+          case irr::EMIE_LMOUSE_LEFT_UP:      on_change( LEFT,   RELEASED ); break;
+          case irr::EMIE_RMOUSE_LEFT_UP:      on_change( RIGHT,  RELEASED ); break;
+          case irr::EMIE_MMOUSE_LEFT_UP:      on_change( MIDDLE, RELEASED ); break;
+
+          case irr::EMIE_MOUSE_WHEEL: wheelPos += e.MouseInput.Wheel;
+
+          case irr::EMIE_MOUSE_MOVED: 
+            position.X = e.MouseInput.X;
+            position.Y = e.MouseInput.Y;
+        }
+    }
+
+    return false;
+}
+
+bool Mouse::button_pressed( Buttons b )
+{
+    return buttons[ b ] & ( PRESSED | ACTIVE );
+}
+
+bool Mouse::button_released( Buttons b )
+{
+    return buttons[ b ] & ( RELEASED | ACTIVE );
+}
+
+bool Mouse::button_down( Buttons b )
+{
+    return buttons[ b ] & PRESSED;
 }
--- Input.h
+++ Input.h
@@ -2,14 +2,38 @@
 #include <irrlicht/irrlicht.h>
 #include <algorithm>
 
+enum KeySate 
+{
+    PRESSED  = 1,
+    RELEASED = 2,
+    ACTIVE   = 4 // Was it pressed or released this frame?
+};
+
 struct Keyboard : public irr::IEventReceiver
 {
     bool states[ irr::KEY_KEY_CODES_COUNT ];
 
     Keyboard();
 
-    virtual bool OnEvent( const irr::SEvent& e );
+    bool OnEvent( const irr::SEvent& e );
 
     bool key_pressed( irr::EKEY_CODE c );
     bool key_pressed( char c ); // Works for a-z, 0-9, whitespace.
+};
+
+struct Mouse : public irr::IEventReceiver
+{
+    enum Buttons { LEFT, RIGHT, MIDDLE, N_BUTTONS };
+
+    irr::core::position2di position;
+    KeySate buttons[ N_BUTTONS ];
+    float wheelPos;
+
+    Mouse();
+
+    bool OnEvent( const irr::SEvent& e );
+
+    bool button_pressed(  Buttons b );
+    bool button_released( Buttons b );
+    bool button_down(     Buttons b );
 };
